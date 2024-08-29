865e0d3bbfb8498570a22ef9680791ec7a3d9c62
WittySage <wittysage143@gmail.com>
2024-08-30 00:00:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,16 @@ var typingEffect = new Typed(".typedText", {
 })
 
 ScrollReveal({ reset: true,
-    distance: '60px',
+    distance: '100px',
     duration: 2500,
     delay: 400
  });
 
 ScrollReveal().reveal(".text-info", { delay: 500, origin: 'left'})
-ScrollReveal().reveal(".home-img", { delay: 600, origin: 'bottom'})
-ScrollReveal().reveal("social-icons", { delay: 900, origin: 'bottom'})
-ScrollReveal().reveal(".text-info", { delay: 500, origin: 'left'})
+ScrollReveal().reveal(".home-img", { delay: 500, origin: 'bottom'})
+ScrollReveal().reveal("social-icons", { delay: 900, origin: 'bottom', interval: 200 })
+ScrollReveal().reveal(".bx", { delay: 500, origin: 'left'})
+ScrollReveal().reveal(".btn-group", { delay: 900, origin: 'bottom'})
+ScrollReveal().reveal(".about-img", { delay: 500, origin: 'bottom'})
+ScrollReveal().reveal(".about-content", { delay: 500, origin: 'bottom'})
+ScrollReveal().reveal(".skill", { delay: 800, origin: 'right'})
