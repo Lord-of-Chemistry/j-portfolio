e3b4473a597bd1f3b3faae13e658b0af8771cfa7
WittySage <wittysage143@gmail.com>
2024-08-30 16:52:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,16 +20,18 @@ var typingEffect = new Typed(".typedText", {
 })
 
 ScrollReveal({ reset: true,
-    distance: '100px',
-    duration: 2500,
+    distance: '300px',
+    duration: 2300,
     delay: 400
  });
 
 ScrollReveal().reveal(".text-info", { delay: 500, origin: 'left'})
 ScrollReveal().reveal(".home-img", { delay: 500, origin: 'bottom'})
-ScrollReveal().reveal("social-icons", { delay: 900, origin: 'bottom', interval: 200 })
+ScrollReveal().reveal("social-icons", { delay: 500, origin: 'bottom', interval: 200 })
 ScrollReveal().reveal(".bx", { delay: 500, origin: 'left'})
-ScrollReveal().reveal(".btn-group", { delay: 900, origin: 'bottom'})
+ScrollReveal().reveal(".unique", { delay: 500, origin: 'bottom'})
 ScrollReveal().reveal(".about-img", { delay: 500, origin: 'bottom'})
 ScrollReveal().reveal(".about-content", { delay: 500, origin: 'bottom'})
-ScrollReveal().reveal(".skill", { delay: 800, origin: 'right'})
+ScrollReveal().reveal(".skill", { delay: 500, origin: 'right'})
+ScrollReveal().reveal(".heading", { delay: 500, origin: 'top'})
+ScrollReveal().reveal(".services-info", { delay: 500, origin: 'right'})
